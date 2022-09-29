be860328f335310c0b737e0eebdbec5380027b4b
Maksim Lin <maks@manichord.com>
2022-09-30 09:40:47 +1000

    update linux build generated files

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -2,15 +2,17 @@
 //  Generated file. Do not edit.
 //
 
+// clang-format off
+
 #include "generated_plugin_registrant.h"
 
-#include <file_chooser/file_chooser_plugin.h>
+#include <file_selector_linux/file_selector_plugin.h>
 #include <window_size/window_size_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
-  g_autoptr(FlPluginRegistrar) file_chooser_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "FileChooserPlugin");
-  file_chooser_plugin_register_with_registrar(file_chooser_registrar);
+  g_autoptr(FlPluginRegistrar) file_selector_linux_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FileSelectorPlugin");
+  file_selector_plugin_register_with_registrar(file_selector_linux_registrar);
   g_autoptr(FlPluginRegistrar) window_size_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "WindowSizePlugin");
   window_size_plugin_register_with_registrar(window_size_registrar);
